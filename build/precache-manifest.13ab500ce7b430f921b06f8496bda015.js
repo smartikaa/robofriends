fd82c2e8b07a560373fb16214d01cfcff9709f04
Sahil <soilshubham@gmail.com>
2020-08-12 14:47:31 +0530

    built the app

---
+++ build/precache-manifest.13ab500ce7b430f921b06f8496bda015.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c06d06be8f072f03130f8889f1599ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f10ba60a7940442a9ca8",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0772065b5c8f703c9a71",
+    "url": "/static/css/main.2e890f8a.chunk.css"
+  },
+  {
+    "revision": "f10ba60a7940442a9ca8",
+    "url": "/static/js/2.d5f66d5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d5f66d5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0772065b5c8f703c9a71",
+    "url": "/static/js/main.e38d2ae6.chunk.js"
+  },
+  {
+    "revision": "df3626599cd0ef421114",
+    "url": "/static/js/runtime-main.75482288.js"
+  }
+]);
